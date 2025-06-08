777214c8540cd062f17d8c9b98aa36969a96d947
WiktoriaOpalinska1 <117988889+WiktoriaOpalinska1@users.noreply.github.com>
2025-06-08 12:46:11 +0200

    Improve formatting in statisticsdialog.cpp

--- Manager_projektow/statisticsdialog.cpp
+++ Manager_projektow/statisticsdialog.cpp
@@ -3,13 +3,11 @@
 
 StatisticsDialog::StatisticsDialog(const QStringList& stats, QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::StatisticsDialog)
-{
+    ui(new Ui::StatisticsDialog){
     ui->setupUi(this);
     ui->listWidget->addItems(stats);
 }
 
-StatisticsDialog::~StatisticsDialog()
-{
+StatisticsDialog::~StatisticsDialog(){
     delete ui;
 }
